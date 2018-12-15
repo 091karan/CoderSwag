b9bae90c1f04a3e6841af017ad63bf8009f2387c
Karan Agarwal <091karan@gmail.com>
2018-12-15 18:02:47 +0530

    Add RecylerView and Adpater for Products.

---
+++ app/src/main/java/com/karan/android/coderswag/adapters/ProductsAdapter.kt
@@ -0,0 +1,43 @@
+package com.karan.android.coderswag.adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.karan.android.coderswag.R
+import com.karan.android.coderswag.model.Product
+import kotlinx.android.synthetic.main.products_list_item.view.*
+import java.text.FieldPosition
+
+class ProductsAdapter(val context: Context,val products: List<Product>) : RecyclerView.Adapter<ProductsAdapter.Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(context).inflate(R.layout.products_list_item,parent,false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return products.count()
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bindProduct(products[position],context)
+    }
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val productImage = itemView.findViewById<ImageView>(R.id.productImage)
+        val productName = itemView.findViewById<TextView>(R.id.productName)
+        val productPrice = itemView.findViewById<TextView>(R.id.productCost)
+
+        fun bindProduct(product: Product,context: Context){
+            val imageResourceId = context.resources.getIdentifier(product.image,"drawable",context.packageName)
+            productImage.setImageResource(imageResourceId)
+            productName.text = product.title
+            productPrice.text = product.price
+        }
+
+    }
+
+}
--- app/src/main/java/com/karan/android/coderswag/controller/ProductsActivity.kt
+++ app/src/main/java/com/karan/android/coderswag/controller/ProductsActivity.kt
@@ -1,16 +1,42 @@
 package com.karan.android.coderswag.controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import com.karan.android.coderswag.R
+import com.karan.android.coderswag.adapters.ProductsAdapter
+import com.karan.android.coderswag.services.DataService
 import com.karan.android.coderswag.utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var productsAdapter: ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
         val categoryName = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryName)
+
+        productsAdapter = ProductsAdapter(this,DataService.getProducts(categoryName))
+        productsItemView.adapter = productsAdapter
+
+        var spanCount = 2
+
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount=3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if(screenSize >= 720){
+            spanCount=3
+        }
+
+        productsItemView.layoutManager = GridLayoutManager(this,spanCount)
+
+        productsItemView.setHasFixedSize(true)
+
     }
 }
