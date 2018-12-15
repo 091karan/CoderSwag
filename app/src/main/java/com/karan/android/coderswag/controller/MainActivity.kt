dbd097173a8fd18b23466b6e29d3d7df720aee5a
Karan Agarwal <091karan@gmail.com>
2018-12-15 12:46:44 +0530

    Add Adapter to the list view of Category page

--- app/src/main/java/com/karan/android/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/karan/android/coderswag/controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.karan.android.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.karan.android.coderswag.R
+import com.karan.android.coderswag.model.Category
+import com.karan.android.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,DataService.categories)
+        listItemView.adapter = adapter
+
     }
 }
